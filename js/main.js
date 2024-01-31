8ee76a575dba1f8bc7e9d7a3dac160c4de288340
Artem Voron <artemvoron@gmail.com>
2024-02-01 00:55:05 +0300

    add main header & first screen

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const citySelectBtn = document.querySelector('.city-select')
+const searchPanel = document.querySelector('.header__search-panel')
+
+
+document.addEventListener('click', checkClick)
+
+function checkClick(event) {
+    if (event.target.closest('.city-select')) {
+        openCityList()
+        closeSearchPanel()
+    }
+
+    else if (event.target.closest('.header__search-cancel ')) {
+        closeSearchPanel()
+    }
+    else if (event.target.closest('.header__main-search') || event.target.closest('.header__search-panel')) {
+        closeCitylist()
+        openSearchPanel()
+    }
+    else {
+        closeCitylist()
+        closeSearchPanel()
+    }
+}
+
+function openCityList() {
+    citySelectBtn.classList.add('active')
+}
+
+function closeCitylist() {
+    citySelectBtn.classList.remove('active')
+}
+
+function openSearchPanel() {
+    searchPanel.classList.add('active')
+}
+
+function closeSearchPanel() {
+    searchPanel.classList.remove('active')
+}
